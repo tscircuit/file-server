835248a1af211e118c3ab3030f70cd7f08eb53ac
snkk2x-collab <snkk2x@gmail.com>
2026-06-18 23:22:32 +0800

    docs: document file download routes

--- routes/index.ts
+++ routes/index.ts
@@ -15,6 +15,8 @@ This is a simple file server API, it has the following API:
 /files/get?file_path=... - Get a file
 /files/list - List all files
 /files/upsert - Upsert a file
+/files/download?file_path=... - Download a file as an attachment
+/files/download/path/to/file.txt - Download a file by path as an attachment
 
 /events/list?since=... - List events since a given timestamp
 /events/list?event_type=... - List events filtered by event type
